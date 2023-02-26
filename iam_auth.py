1c8832d28cbdef78a709168e3add3da968c2a4b9
kmnormand <kmnormand@yahoo.com>
2023-02-25 21:11:39 -0600

    updated to use get method and removed debugging print statements

--- iam_auth.py
+++ iam_auth.py
@@ -11,16 +11,14 @@ response = client.get_account_authorization_details(
     Filter = ["User", "Group"]
 )
 
-# print(response)
-
 user_list = [x['UserName'] for x in response['UserDetailList']]
 print(user_list)
 for user in response['UserDetailList']:
     username = user['UserName']
-    if (len(user['UserName']['AttachedManagedPolicies'])) !=0:
-        managed_policies = user['UserName']['AttachedManagedPolicies']
-        user_dict[username].append(managed_policies)
-    # print(user)
+    if user.get("AttachedManagedPolicies"):
+        user_dict[username].append(user.get("AttachedManagedPolicies"))
+    else:
+        print("Empty Managed Policy")
 
 print(user_dict)
 group_list = [x['GroupName'] for x in response['GroupDetailList']]
